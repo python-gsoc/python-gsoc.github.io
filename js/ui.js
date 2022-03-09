60b0585ed8a44d60ba4c46cac0039bda6982d3c9
botanicvelious <cyberempires@gmail.com>
2022-03-09 03:20:31 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -44,3 +44,25 @@
     };
 
 })(this, this.document);
+
+function shuffle() {
+  var container = document.getElementById("container");
+  var elementsArray = Array.prototype.slice.call(container.getElementsByClassName('shuffleMe'));
+	elementsArray.forEach(function(element){
+  	container.removeChild(element);
+  })
+  shuffleArray(elementsArray);
+  elementsArray.forEach(function(element){
+  container.appendChild(element);
+})
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
