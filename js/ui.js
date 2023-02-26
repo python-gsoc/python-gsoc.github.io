f50d8193edef5851296c2844a80c8298f1c54a5b
0rigin-c0de <shunnu807@gmail.com>
2023-02-26 13:03:33 +0530

    update eventlistener, toggleClass, improve readability

--- js/ui.js
+++ js/ui.js
@@ -1,68 +1,60 @@
 (function (window, document) {
+  var layout = document.getElementById("layout"),
+    menu = document.getElementById("menu"),
+    menuLink = document.getElementById("menuLink"),
+    contents = document.querySelectorAll(".main");
 
-    var layout   = document.getElementById('layout'),
-        menu     = document.getElementById('menu'),
-        menuLink = document.getElementById('menuLink'),
-        content  = document.getElementsByClassName('main');
+  function toggleClass(element, className) {
+    var classes = element.className.split(/\s+/),
+      index = classes.indexOf(className);
 
-    function toggleClass(element, className) {
-        var classes = element.className.split(/\s+/),
-            length = classes.length,
-            i = 0;
-
-        for(; i < length; i++) {
-          if (classes[i] === className) {
-            classes.splice(i, 1);
-            break;
-          }
-        }
-        // The className is not found
-        if (length === classes.length) {
-            classes.push(className);
-        }
-
-        element.className = classes.join(' ');
+    if (index >= 0) {
+      classes.splice(index, 1);
+    } else {
+      classes.push(className);
     }
 
-    function toggleAll(e) {
-        var active = 'active';
+    element.className = classes.join(" ");
+  }
 
-        e.preventDefault();
-        toggleClass(layout, active);
-        toggleClass(menu, active);
-        toggleClass(menuLink, active);
-    }
+  function toggleAll(e) {
+    e.preventDefault();
+    toggleClass(layout, "active");
+    toggleClass(menu, "active");
+    toggleClass(menuLink, "active");
+  }
 
-    menuLink.onclick = function (e) {
+  menuLink.addEventListener("click", toggleAll);
+
+  contents.forEach(function (content) {
+    content.addEventListener("click", function (e) {
+      if (menu.classList.contains("active")) {
         toggleAll(e);
-    };
-
-    content.onclick = function(e) {
-        if (menu.className.indexOf('active') !== -1) {
-            toggleAll(e);
-        }
-    };
-
-})(this, this.document);
+      }
+    });
+  });
+})(window, document);
 
 function shuffle() {
   var container = document.getElementById("container");
-  var elementsArray = Array.prototype.slice.call(container.getElementsByClassName('shuffleMe'));
-	elementsArray.forEach(function(element){
-  	container.removeChild(element);
-  })
+  if (!container) return;
+
+  var elementsArray = Array.from(container.getElementsByClassName("shuffleMe"));
+  if (elementsArray.length === 0) return;
+
   shuffleArray(elementsArray);
-  elementsArray.forEach(function(element){
-  container.appendChild(element);
-})
+  elementsArray.forEach(function (element) {
+    container.appendChild(element);
+  });
 }
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    return array;
+  array = array.slice(); // Make a copy of the original array
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
 }
